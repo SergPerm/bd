da9b2b045d6cd66cbc481e259a111cca00a30505
SergPerm <48542126+SergPerm@users.noreply.github.com>
2021-09-15 00:08:48 +0500

    Add files via upload

---
+++ bdFauna/insert.sql
@@ -0,0 +1,21 @@
+INSERT INTO public.fauna(name, avg_age, discovery_date)
+	VALUES ('tiger', 10000, date '15.01.1230');
+INSERT INTO public.fauna(name, avg_age, discovery_date)
+	VALUES ('puma', 20000, date '26.08.1437');
+INSERT INTO public.fauna(name, avg_age, discovery_date)
+	VALUES ('gold fish', 2000, date '07.04.1876');
+INSERT INTO public.fauna(name, avg_age, discovery_date)
+	VALUES ('spider', 1000, null);
+INSERT INTO public.fauna(name, avg_age, discovery_date)
+	VALUES ('black widow', 1500, date '01.01.1500');		
+INSERT INTO public.fauna(name, avg_age, discovery_date)
+	VALUES ('horse', 15000, date '15.10.1951');
+INSERT INTO public.fauna(name, avg_age)
+	VALUES ('turtle', 40000);
+INSERT INTO public.fauna(name, avg_age, discovery_date)
+	VALUES ('elefant', 21000, date '01.01.1950');
+INSERT INTO public.fauna(name, avg_age, discovery_date)
+	VALUES ('homo sapiens', 30000, date '14.10.1900');
+INSERT INTO public.fauna(name, avg_age, discovery_date)
+	VALUES ('shark', 10500, date '1950-10-25');
+	
