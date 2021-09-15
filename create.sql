6f60cddcc91af30f831ea53e40fd4de2c5607ea6
SergPerm <48542126+SergPerm@users.noreply.github.com>
2021-09-15 20:11:18 +0500

    Add files via upload

---
+++ create.sql
@@ -0,0 +1,57 @@
+create table role (
+    id serial primary key,
+    name varchar(100),
+	description text
+);
+
+create table rule(
+    id serial primary key,
+    name varchar(100),
+	description text
+);
+
+create table role_rules (
+    id serial primary key,
+    name varchar(255),	
+    role_id int references role(id),
+    rules_id int references rules(id),
+	unique(role_id, rules_id)
+);
+
+create table users (
+    id serial primary key,
+	first_name varchar(100),
+	last_name varchar(100),
+	age int,
+	role_id int references role(id)
+);
+
+create table category (
+    id serial primary key,
+    name varchar(255)
+);
+
+create table state (
+    id serial primary key,
+    name varchar(100)	
+);
+
+create table item(
+    id serial primary key,
+    name varchar(255),	
+    users_id int references users(id),
+    state_id int references state(id),
+	category_id int references category(id)
+);
+
+create table comments (
+    id serial primary key,
+    comment text,	
+    item_id int references item(id)
+);
+
+create table attachs (
+    id serial primary key,
+    name varchar(255),	
+    item_id int references item(id)
+);
